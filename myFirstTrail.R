a82e5e20a361d92524f73f784113825e3a229076
Cyrus <gautam.cyrus80@gmail.com>
2020-11-05 02:43:00 -0800

    fiiddling around

--- myFirstTrail.R
+++ myFirstTrail.R
@@ -1,2 +1,9 @@
-install.packages(airmiles)
-airmiles
+install.packages(cars)
+cars
+cars$speed
+data <- c(1, 2, 3)
+for (i in cars$speed){
+  if (i %% 2 == 0) 
+    newData <- append(data, i)
+  }
+print(newData)
